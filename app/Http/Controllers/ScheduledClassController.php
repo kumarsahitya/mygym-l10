e39c979516878709bc4e4615a9a275e49333c49b
Kumar Sahitya <ksahitya27@gmail.com>
2024-02-23 14:15:01 +0530

    Added Policy for delete scheduled class

--- app/Http/Controllers/ScheduledClassController.php
+++ app/Http/Controllers/ScheduledClassController.php
@@ -56,9 +56,12 @@ class ScheduledClassController extends Controller
      */
     public function destroy(ScheduledClass $schedule)
     {
-        if (auth()->user()->id !== $schedule->instructor_id) {
+        if (auth()->user()->cannot('delete', $schedule)) {
             abort(403);
         }
+        // if (auth()->user()->id !== $schedule->instructor_id) {
+        //     abort(403);
+        // }
 
         $schedule->delete();
 
