61a82187435b3c4fa44cf0dd35759432747dfe4f
Kumar Sahitya <ksahitya27@gmail.com>
2024-02-26 13:23:47 +0530

    many-to-many relation, user can book scheduled class

--- database/factories/ScheduledClassFactory.php
+++ database/factories/ScheduledClassFactory.php
@@ -20,7 +20,7 @@ class ScheduledClassFactory extends Factory
         return [
             'instructor_id' => rand(15, 24),
             'class_type_id' => rand(1, 4),
-            'date_time' => Carbon::now()->addHours(rand(24, 120))->minutes(0)->seconds(0),
+            'date_time' => Carbon::now()->addHours(rand(24, 240))->minutes(0)->seconds(0),
         ];
     }
 }
